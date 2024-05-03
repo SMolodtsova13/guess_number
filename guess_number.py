84c6e58faf374f63369651f0ff9ce98cdefa8528
Светлана Молодцова <molodtsova201@yandex.ru>
2024-05-03 22:07:05 +0300

    refactoring

--- guess_number.py
+++ guess_number.py
@@ -4,14 +4,14 @@ number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
 while True:
-        guess = int(input('Введите число: '))
-    
-        if guess < number:
-            print('Ваше число меньше того, что загадано.')
-    
-        elif guess > number:
-            print('Ваше число больше того, что загадано.')
-    
-        elif guess == number:
-            break
+    guess = int(input('Введите число: '))
+
+    if guess < number:
+        print('Ваше число меньше того, что загадано.')
+
+    elif guess > number:
+        print('Ваше число больше того, что загадано.')
+
+    elif guess == number:
+        break
 print('Отличная интуиция! Вы угадали число :)')
